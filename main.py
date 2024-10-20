08001c2169dfc0e898bdfcfa03b50e8c2d45452e
avengestaves <avengestaves@proton.me>
2024-10-20 10:20:59 -0400

    Added the first primitive gameloop

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
+from constants import *
 
 def main():
-    print(f"Starting asteroids!")
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    #print(f"Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
